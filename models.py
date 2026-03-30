a569727ba3a2c9594609c6cc757f43d286855dc8
eliasalqopaty-art <eliasalqopaty@gmail.com>
2026-03-31 01:17:47 +0300

    models.py

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
-class Patient:
-    def __init__(self, patient_id, name, history):
-        self.patient_id = patient_id
-        self.name = name
-        self.history = history
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from database import Base
 
-
-    def get_summary(self):
-        return f"المريض: {self.name} - السجل: {self.history}"
+class User(Base):
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False) # كلمة السر المشفرة
+    # الربط بالشركة (كل مستخدم ينتمي لشركة واحدة فقط)
+    company_id = Column(UUID(as_uuid=True), ForeignKey("companies.id"))
