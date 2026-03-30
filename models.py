d858132569949da8757ce9df60c464d76801b0e5
eliasalqopaty-art <eliasalqopaty@gmail.com>
2026-03-31 00:47:32 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,19 +1,8 @@
-from sqlalchemy import Column, String, Boolean, Date, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
-import uuid
-from database import Base
+class Patient:
+    def __init__(self, patient_id, name, history):
+        self.patient_id = patient_id
+        self.name = name
+        self.history = history
 
-class Company(Base):
-    __tablename__ = "companies"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    company_name = Column(String, nullable=False)
-    api_key = Column(String, unique=True)
-
-class Batch(Base):
-    __tablename__ = "batches"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    company_id = Column(UUID(as_uuid=True), ForeignKey("companies.id"))
-    medicine_name = Column(String, nullable=False)
-    batch_num = Column(String, nullable=False)
-    expiry_date = Column(Date, nullable=False)
-    is_recalled = Column(Boolean, default=False)
+    def get_summary(self):
+        return f"المريض: {self.name} - السجل: {self.history}"
